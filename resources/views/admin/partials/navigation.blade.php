044bf5a5d2b5a86219993e272ebbca995efdf5de
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2017-06-26 14:23:45 +0100

    Navigation n+1 queries fix

--- resources/views/admin/partials/navigation.blade.php
+++ resources/views/admin/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar">
     <section class="sidebar">
-        <div class="user-panel">
+        <div class="user-panel hide">
             <div class="pull-left image">
                 <img src="{{ profile_image() }}" class="img-circle" alt="User Image">
             </div>
@@ -12,8 +12,8 @@
 
         <nav>
             <ul class="sidebar-menu" data-widget="tree">
-                <li class="header">MAIN NAVIGATION</li>
-                {!! $navigation !!}
+                <li class="header">NAVIGATION</li>
+                @include ('admin.partials.navigation_list', ['collection' => $navigation['root']])
             </ul>
         </nav>
     </section>
