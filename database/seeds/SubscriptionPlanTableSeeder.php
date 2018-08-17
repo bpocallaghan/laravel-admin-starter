8a01a298a520ba690704d3d5a9ef72772abebba7
AkkuFresh <AkkuFresh@users.noreply.github.com>
2018-08-17 14:06:05 +0200

    Remove DB::delete

--- database/seeds/SubscriptionPlanTableSeeder.php
+++ database/seeds/SubscriptionPlanTableSeeder.php
@@ -11,7 +11,7 @@ class SubscriptionPlanTableSeeder extends Seeder
     public function run()
     {
         SubscriptionPlan::truncate();
-        DB::delete('TRUNCATE subscription_plan_feature_pivot');
+        //DB::delete('TRUNCATE subscription_plan_feature_pivot');
 
         $csvPath = database_path() . DIRECTORY_SEPARATOR . 'seeds' . DIRECTORY_SEPARATOR . 'csv' . DIRECTORY_SEPARATOR . 'subscription_plans.csv';
         $items = csv_to_array($csvPath);
