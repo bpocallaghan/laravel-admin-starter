00c484123f37d6da9f06fe8e60e5b376b74f70b3
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2019-11-29 20:39:50 +0200

    Delete All

--- resources/views/partials/analytics.blade.php
+++
@@ -1,7 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('app.google_analytics') }}"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '{{ config('app.google_analytics') }}');
-</script>
